fdbd627ef3eb08e28611b4c128d969c50869918d
Siew Joe Kane <joekanesiew@outlook.com>
2021-09-03 11:53:44 +0800

    added program files

---
+++ DotnetCleanArchitectureTemplate-master/src/Infrastructure/Persistence/ApplicationDbContextSeed.cs
@@ -0,0 +1,47 @@
+﻿using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Persistence
+{
+    public static class ApplicationDbContextSeed
+    {
+        public static async Task SeedDefaultUserAsync(UserManager<ApplicationUser> userManager)
+        {
+            var defaultUser = new ApplicationUser { UserName = "jakeAdminLocal@gmail.com", Email = "jakeAdminLocal@gmail.com" };
+
+            if (userManager.Users.All(u => u.UserName != defaultUser.UserName))
+            {
+                await userManager.CreateAsync(defaultUser, "welcome123");
+                await userManager.AddToRoleAsync(defaultUser, "Administrator");
+            }
+        }
+
+        public static async Task SeedSampleDataAsync(ApplicationDbContext context)
+        {
+            // Seed, if necessary
+            if (!context.TodoLists.Any())
+            {
+                context.TodoLists.Add(new TodoList
+                {
+                    Title = "Shopping",
+                    Items =
+                    {
+                        new TodoItem { Title = "Apples", Done = true },
+                        new TodoItem { Title = "Milk", Done = true },
+                        new TodoItem { Title = "Bread", Done = true },
+                        new TodoItem { Title = "Toilet paper" },
+                        new TodoItem { Title = "Pasta" },
+                        new TodoItem { Title = "Tissues" },
+                        new TodoItem { Title = "Tuna" },
+                        new TodoItem { Title = "Water" }
+                    }
+                });
+
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ DotnetCleanArchitectureTemplate-master/src/Infrastructure/Persistence/Migrations/20210612065157_Remove User Photo.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchitecture.Infrastructure.Persistence.Migrations
+{
+    public partial class RemoveUserPhoto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserPhoto",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserPhoto",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
