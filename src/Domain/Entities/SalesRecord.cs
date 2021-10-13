1b54c4a9a8a3345afa5bd34fc82578587eb352a1
Melvin Hoh <76925986+melvinhoh@users.noreply.github.com>
2021-10-13 21:00:32 +0800

    sales record wip

---
+++ src/Domain/Entities/SalesRecord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CleanArchitecture.Domain.Common;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public class SalesRecord : AuditableEntity
+    {
+        public int _salesId, _employeeId, _itemId, _quantity;
+        public DateTime _date;
+        public string _remarks;
+        public bool _isDeleted;
+        public SalesRecord() { }
+        public int SalesId { get => _salesId; }
+        public DateTime Date { get => _date; }
+        public int EmployeeId { get => _employeeId; }
+        public int ItemId { get => _itemId; }
+        public int Quantity { get => _quantity; set => _quantity = value; }
+        public string Remarks { get => _remarks; set => _remarks = value; }
+        public bool IsDeleted { get => _isDeleted; set => _isDeleted = value; }
+    }
+}
