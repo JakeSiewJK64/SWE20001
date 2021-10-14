e1e7ea46f65f1533d737d8715cd582edcfa2b0bd
Siew Joe Kane <joekanesiew@outlook.com>
2021-10-14 08:53:58 +0800

    added item enums

---
+++ src/Domain/Enums/ItemEnums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Domain.Enums
+{
+    public enum Status
+    {
+        Full = 0,
+        OutOfStock = 1,
+        Stale = 2,
+        SellingFast = 3,
+    }
+}
