f43aafed22fe03d91a3b1259268cc65a6f2721f8
Melvin Hoh <76925986+melvinhoh@users.noreply.github.com>
2021-10-14 22:14:24 +0800

    sales api wip

---
+++ src/Application/Sales/Commands/GetSales/PriorityLevelDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArchitecture.Application.Sales.Commands.GetSales
+{
+    public class PriorityLevelDto
+    {
+        public int Value { get; set; }
+
+        public string Name { get; set; }
+    }
+}
