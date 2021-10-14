ef7bec6c10c18c4785e9da0f65b674585be6a4a5
Melvin Hoh <76925986+melvinhoh@users.noreply.github.com>
2021-10-14 22:14:24 +0800

    sales api wip

---
+++ src/Application/Sales/Commands/GetSales/SalesDto.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using CleanArchitecture.Application.Common.Mappings;
+using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Application.Sales.Commands.GetSales
+{
+    public class SalesDto : IMapFrom<SalesRecord>
+    {
+        public int _salesRecordId { get; set; }
+        public int _employeeId { get; set; }
+        private int _itemId { get; set; }
+        private int _quantity { get; set; }
+        private DateTime _date { get; set; }
+        private string _remarks { get; set; }
+        private bool _isDeleted { get; set; }
+
+    }
+}
