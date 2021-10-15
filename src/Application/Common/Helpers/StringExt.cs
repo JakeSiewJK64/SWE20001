07579434c5b13aac1c2cd2dacb3ad6658a2f5acd
Siew Joe Kane <joekanesiew@outlook.com>
2021-10-15 13:59:26 +0800

    updated string extension

--- src/Application/Common/Helpers/StringExt.cs
+++ src/Application/Common/Helpers/StringExt.cs
@@ -1,4 +1,8 @@
-﻿namespace CleanArchitecture.Application.Common.Helpers
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+
+namespace CleanArchitecture.Application.Common.Helpers
 {
     public static class StringExt
     {
@@ -11,6 +15,46 @@
         public static string ConvertNullOrEmptyTo(this string value, string defaultValue)
         {
             return string.IsNullOrEmpty(value) ? defaultValue : value;
+        }
+
+        public static List<string> ToStringList(this string value)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    return value.Split(";").ToList();
+                }
+            }
+            catch (System.Exception)
+            {
+            }
+
+            return new List<string>();
+        }
+
+        public static T ToObject<T>(this string value)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    return JsonSerializer.Deserialize<T>(value, new JsonSerializerOptions
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                    });
+                }
+            }
+            catch (System.Exception)
+            {
+            }
+
+            return default;
+        }
+
+        public static string NullToDash(this string value)
+        {
+            return string.IsNullOrEmpty(value) ? "-" : value;
         }
     }
 }
