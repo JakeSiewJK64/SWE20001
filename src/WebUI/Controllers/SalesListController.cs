cb375bc2788fdd379c69267cf971bd6410ff4dad
Siew Joe Kane <joekanesiew@outlook.com>
2021-10-19 15:35:10 +0800

    restructure sales list controller

--- src/WebUI/Controllers/SalesListController.cs
+++ src/WebUI/Controllers/SalesListController.cs
@@ -1,33 +1,32 @@
 using CleanArchitecture.Application.Sales.Commands.GetSales;
 using CleanArchitecture.Application.Sales.Commands.GenerateSalesReport;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using CleanArchitecture.Application.Sales.Queries.GetAllSalesQuery;
+using CleanArchitecture.Application.Sales.Queries.GetSalesByIdQuery;
+using CleanArchitecture.Application.Sales.Queries.UpsertSalesCommand;
 
 namespace CleanArchitecture.WebUI.Controllers
 {
     public class SalesListController : ApiController
     {
         [HttpGet]
-        public async Task<ActionResult<List<SalesDto>>> GetAllSalesRecordsQuery()
-        {
-            return await Mediator.Send(new GetAllSalesQuery());
-        }
+        public async Task<ActionResult<List<SalesDto>>> GetAllSalesRecordsQuery() 
+            => Ok(await Mediator.Send(new GetAllSalesQuery()));
 
         [HttpGet]
-        public async Task<ActionResult<List<SalesDto>>> GetSalesByIdQuery(string searchCriteria) 
+        public async Task<ActionResult<List<SalesDto>>> GetSalesByIdQuery(string searchCriteria)
             => Ok(await Mediator.Send(new GetSalesByIdQuery { SearchCriteria = searchCriteria }));
 
         [HttpPost]
         public async Task<ActionResult<int>> UpsertSalesCommand(SalesDto salesDto)
             => Ok(await Mediator.Send(new UpsertSalesCommand { salesObj = salesDto }));
 
-            [HttpGet("{date}")]
+        [HttpGet("{date}")]
         public async Task<FileResult> Get(string date)
         {
             var vm = await Mediator.Send(new ExportSalesReportQuery { Date = date });
-
             return File(vm.Content, vm.ContentType, vm.FileName);
         }
     }
