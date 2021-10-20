fb7760e06fd94aa70a4697bbf9994965766d14a5
Melvin Hoh <76925986+melvinhoh@users.noreply.github.com>
2021-10-20 22:19:12 +0800

    predict group is sales items query wip

---
+++ src/Application/Common/Models/SalesGroupItemModel.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Application.Common.Models
+{
+    public class SalesGroupItemModel
+    {
+        public int ItemId { get; set; }
+        public Item Item { get; set; }
+        public int PredictedSales { get; set; }
+    }
+}
