6ed26d1b10080bba6af825c9d7a316222291a13b
JustinL04 <justinleong04@gmail.com>
2021-10-20 13:47:47 +0800

    GetItems API (WIP)

---
+++ src/WebUI/Controllers/ItemsListController.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CleanArchitecture.Application.Items.GetAllItemsQuery;
+
+using System;
+
+using CleanArchitecture.Application.Common.Models;
+
+namespace CleanArchitecture.WebUI.Controllers
+{
+    public class ItemsListController : ApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<ItemsDto>>> GetAllItemsQuery()
+            => Ok(await Mediator.Send(new GetAllItemsQuery()));
+
+        
+    }
+}
