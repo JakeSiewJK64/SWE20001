f48d4b994d31a1bb7a7ac7eea7559635647f218f
Siew Joe Kane <joekanesiew@outlook.com>
2021-10-29 21:50:32 +0800

    + updated inventory page + updated sales to add sorting
    + updated inventory page
    + updated sales to add sorting

--- src/WebUI/ClientApp/src/app/inventory/inventory.component.ts
+++ src/WebUI/ClientApp/src/app/inventory/inventory.component.ts
@@ -1,13 +1,11 @@
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { ItemDetailsComponentComponent } from '../inventory-dialogs/item-details-component/item-details-component.component';
-import { AuditableEntity, Item, ItemCategory } from 'src/app/cleanarchitecture-api';
-import { MatTableDataSource } from '@angular/material/table';
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { MatTable } from '@angular/material/table';
+import { ItemCategory } from 'src/app/cleanarchitecture-api';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ItemsDto, ItemsListClient } from '../cleanarchitecture-api';
-import { SalesDetailsComponentComponent } from '../sales/_dialogs/sales-details-component/sales-details-component.component';
 import { MatPaginator } from '@angular/material/paginator';
-import { MatSort, Sort } from '@angular/material/sort';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-inventory',
@@ -20,8 +18,16 @@ export class InventoryComponent implements OnInit {
   dataSource: MatTableDataSource<ItemsDto>;
   dialogref: any;
   isLoading: boolean = false;
+  page: number;
+  pageSize: number;
+  totalRecord: number;
 
-  constructor(private dialogservice: MatDialog, private itemService: ItemsListClient) {
+  @ViewChild(MatTable) table: MatTable<ItemsDto>;
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(private dialogservice: MatDialog, 
+    private itemService: ItemsListClient) {
   }
 
   ngOnInit() {
@@ -37,8 +43,20 @@ export class InventoryComponent implements OnInit {
   getItem() {
     this.itemService.getAllItemsQuery().subscribe(x => {
       this.dataSource = new MatTableDataSource(x);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      this.totalRecord = x.length;
       this.isLoading = false;
     });
+  }
+
+  filterItems() {
+  }
+
+  onPageChanged(pageEvent: any) {
+    this.page = pageEvent.pageIndex + 1;
+    this.pageSize = pageEvent.pageSize;
+    this.load();
   }
 
   openAddItemDialog() {
