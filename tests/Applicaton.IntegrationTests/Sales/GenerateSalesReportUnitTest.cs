7714076009fcec08754c29954eac895bf05f3d1b
Siew Joe Kane <joekanesiew@outlook.com>
2021-10-29 22:53:07 +0800

    added unit test for generate sales report

---
+++ tests/Applicaton.IntegrationTests/Sales/GenerateSalesReportUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System;
+using CleanArchitecture.Application.Sales.Commands.GenerateSalesReport;
+
+namespace CleanArchitecture.Application.IntegrationTests.Sales
+{
+    using static Testing;
+    public class GenerateSalesReportUnitTest : TestBase
+    {
+        [Test]
+        public async Task ShouldGenerateSalesReport()
+        {
+            var sales = await SendAsync(new ExportSalesReportQuery
+            {
+                Date = new DateTime()
+            });
+            sales.Should().NotBeNull();
+        }
+    }
+}
