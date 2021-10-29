edd58399a983fe58e600a241ca0c75e2fbbe8d67
Melvin Hoh <76925986+melvinhoh@users.noreply.github.com>
2021-10-29 20:56:43 +0800

    unit tests for sales api wip

---
+++ tests/Applicaton.IntegrationTests/Sales/GetAllSalesCurrentMonthQueryTests.cs
@@ -0,0 +1,50 @@
+﻿using CleanArchitecture.Application.Sales.Commands.GetSales;
+using CleanArchitecture.Application.Sales.Queries.GetAllSalesCurrentMonthQuery;
+using CleanArchitecture.Application.Sales.Queries.GetAllSalesQuery;
+using CleanArchitecture.Application.Sales.Queries.UpsertSalesCommand;
+using CleanArchitecture.Domain.Entities;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.IntegrationTests.TodoItems.Commands
+{
+    using static Testing;
+
+    public class GetAllSalesCurrentMonthQueryTests : TestBase
+    {
+        [Test]
+        public async Task ShouldCreateSalesRecord()
+        {
+            DateTime todayDate = DateTime.Now;
+            DateTime lastMonthDate = todayDate.AddMonths(-1);
+            SalesDto newSalesObj = new SalesDto();
+            newSalesObj._isDeleted = false;
+            newSalesObj._remarks = "bad";
+            newSalesObj._employeeId = "emp1";
+            newSalesObj._salesDate = todayDate;
+            var salesId = await SendAsync(new UpsertSalesCommand
+            {
+                salesObj = newSalesObj
+            });
+
+            SalesDto newSalesObj2 = new SalesDto();
+            newSalesObj2._isDeleted = false;
+            newSalesObj2._remarks = "bad";
+            newSalesObj2._employeeId = "emp1";
+            newSalesObj2._salesDate = lastMonthDate;
+            var salesId2 = await SendAsync(new UpsertSalesCommand
+            {
+                salesObj = newSalesObj2
+            });
+
+            var theSales = await SendAsync(new GetAllSalesCurrentMonthQuery
+            {
+                TimeRange = todayDate
+            });
+
+            theSales.Should().Equals(1);
+        }
+    }
+}
---
+++ tests/Applicaton.IntegrationTests/Sales/UpsertSalesCommandTests.cs
@@ -0,0 +1,67 @@
+﻿using CleanArchitecture.Application.Sales.Commands.GetSales;
+using CleanArchitecture.Application.Sales.Queries.GetAllSalesQuery;
+using CleanArchitecture.Application.Sales.Queries.UpsertSalesCommand;
+using CleanArchitecture.Domain.Entities;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.IntegrationTests.TodoItems.Commands
+{
+    using static Testing;
+
+    public class UpsertSalesCommandTests : TestBase
+    {
+        [Test]
+        public async Task ShouldCreateSalesRecord()
+        {
+            var salesId = await SendAsync(new UpsertSalesCommand
+            {
+                salesObj = new SalesDto()
+            });
+
+            var theSales = await SendAsync(new GetAllSalesQuery
+            {
+
+            });
+            var theSalesId = theSales[0]._salesRecordId;
+
+            var foundSales = await FindAsync<SalesRecord>(theSalesId);
+            foundSales.Should().NotBeNull();
+            foundSales.SalesRecordId.Should().Equals(theSalesId);
+        }
+
+        [Test]
+        public async Task ShouldUpdateSalesRecord()
+        {
+            var salesId = await SendAsync(new UpsertSalesCommand
+            {
+                salesObj = new SalesDto()
+            });
+
+            var theSales = await SendAsync(new GetAllSalesQuery
+            {
+
+            });
+            var theSalesId = theSales[0]._salesRecordId;
+
+            SalesDto newSalesObj = new SalesDto();
+            newSalesObj._isDeleted = false;
+            newSalesObj._remarks = "bad";
+            newSalesObj._employeeId = "emp1";
+
+            var secondSalesId = await SendAsync(new UpsertSalesCommand
+            {
+                salesObj = newSalesObj
+            });
+
+            var foundSales = await FindAsync<SalesRecord>(theSalesId);
+            foundSales.Should().NotBeNull();
+            foundSales.SalesRecordId.Should().Equals(theSalesId);
+            foundSales.IsDeleted.Should().Equals(false);
+            foundSales.Remarks.Should().Equals(newSalesObj._remarks);
+            foundSales.EmployeeId.Should().Equals(newSalesObj._employeeId);
+        }
+    }
+}
