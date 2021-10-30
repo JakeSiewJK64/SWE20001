0a7aae23276f06a02769ee4f62d40d7e2869cb0f
Melvin Hoh <76925986+melvinhoh@users.noreply.github.com>
2021-10-30 19:04:57 +0800

    item details dialog wip

--- src/WebUI/ClientApp/src/app/inventory-dialogs/item-details-component/item-details-component.component.ts
+++ src/WebUI/ClientApp/src/app/inventory-dialogs/item-details-component/item-details-component.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ItemsDto, ItemsListClient, ItemCategory } from 'src/app/cleanarchitecture-api';
+import { ItemsDto, ItemsListClient, ItemCategory, Item } from 'src/app/cleanarchitecture-api';
 
 @Component({
   selector: 'app-item-details-component',
@@ -12,20 +12,37 @@ export class ItemDetailsComponentComponent implements OnInit {
   constructor(private itemService: ItemsListClient,
     private dialogRef: MatDialogRef<ItemDetailsComponentComponent>,
     private snackbar: MatSnackBar,
-    @Inject(MAT_DIALOG_DATA) public data: ItemsDto) {
+    @Inject(MAT_DIALOG_DATA) private data: ItemsDto) {
   }
 
   displayedColumns: string[] = ['ItemID', 'ItemImage', 'Name', 'Type', 'Quantity'];
   dataSource: any;
   ItemType = ItemCategory;
-  model: ItemsDto = new ItemsDto();
+  model: Item = new Item();
 
   ngOnInit() {
     this.load();
   }
 
   load() {
-    if (this.data != undefined || this.data != null) this.model = this.data;
+    if (this.data != undefined || this.data != null) {
+      this.model = this.data;
+      // this.model.itemId = this.data._itemId;
+      // this.model.itemName = this.data._itemName;
+      // this.model.manufacturerName = this.data._manufacturerName;
+      // this.model.manufacturer_Id = this.data._manufacturer_Id;
+      // this.model.costPrice = this.data._costPrice;
+      // this.model.sellPrice = this.data._sellPrice;
+      // this.model.status= this.data._status;
+      // this.model.remarks = this.data._remarks;
+      // this.model.itemCategory = this.data._itemCategory;
+      // this.model.quantity = this.data._quantity;
+      // this.model.batchId = this.data._batchId;
+      // this.model.imageUrl = this.data._imageUrl;
+      // this.model.restockDate = this.data._restockDate;
+      // this.model.expDate = this.data._expDate;
+      // this.model.isDeleted = this.data._isDeleted;
+    }
     console.log(this.model);
   }
 
@@ -33,24 +50,12 @@ export class ItemDetailsComponentComponent implements OnInit {
     const reader = new FileReader();
     reader.readAsDataURL(files[0]);
     reader.onload = (x) => {
-      this.model._imageUrl = x.target.result.toString();
+      this.model.imageUrl = x.target.result.toString();
     }
   }
 
-  save() {
-    this.model._itemCategory;
-    this.model._quantity;
-    this.model._batchId;
-    this.model._manufacturer_Id;
-    this.model._costPrice;
-    this.model._sellPrice;
-    this.model._itemName;
-    this.model._imageUrl;
-    this.model._manufacturerName;
-    this.model._remarks;
-    this.model._restockDate;
-    this.model._expDate;
-    this.model._isDeleted;
+  save(){
+    console.log(this.model);
     this.itemService.upsertItemsCommand(this.model).subscribe(x => {
       console.log(x);
       this.snackbar.open("Item Saved!", "OK", { duration: 5000 });
